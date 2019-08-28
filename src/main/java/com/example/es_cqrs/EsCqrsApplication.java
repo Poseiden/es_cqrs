5747f7d905b15641b3bc83bb49dd804a0eeede56
HaiboZhu <hbzhu@thoughtworks.com>
2019-08-28 23:13:26 +0800

    First blood.

---
+++ src/main/java/com/example/es_cqrs/EsCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.example.es_cqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsCqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsCqrsApplication.class, args);
+	}
+
+}
